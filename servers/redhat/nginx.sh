98e65cebae172c4cabdd21119d0223c95e5828db
Christian DeKonink <sweeper@chrisd.net>
2015-01-03 14:15:33 -0800

    updated to lastest version of nginx

--- servers/redhat/nginx.sh
+++ servers/redhat/nginx.sh
@@ -2,13 +2,18 @@
 #
 # first install nginx
 #
-sudo su -c 'rpm -Uvh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm'
+sudo su -c 'rpm -Uvh http://nginx.org/packages/rhel/6/noarch/RPMS/nginx-release-rhel-6-0.el6.ngx.noarch.rpm'
 yum -y install nginx
 #
 # Install the /etc/nginx/nginx.conf from Configuration Management
 #
 # Install /etc/nginx/conf.d/default.conf from Configuration Management
 #
+# Install /etc/nginx/conf.d/ssl.conf from Configuration Management
+#
+# Install /etc/nginx/conf.d/virtual.conf from Configuration Management
+#
+
 # Create SSL Server Ket and Certificate Signing Request
 #
 # For this i chose godaddy becuase the provide chained domain only certs
